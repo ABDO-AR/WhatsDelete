385c26d729a90f5b32854c1605118b082bde4dd0
ABDO-AR <73670215+ABDO-AR@users.noreply.github.com>
2021-09-17 01:22:30 +0200

    SUPPORTING FILES

--- app/src/main/java/com/ar/team/company/app/whatsdelete/ar/images/ARImagesAccess.java
+++ app/src/main/java/com/ar/team/company/app/whatsdelete/ar/images/ARImagesAccess.java
@@ -23,13 +23,21 @@ public class ARImagesAccess {
     }
 
     // Method(Images):
-    public File[] getWhatsappImagesDirectory() {
+    private File[] getWhatsappImagesDirectory() {
         // Initializing(Paths):
         String externalStorageDirectory = Environment.getExternalStorageDirectory().getAbsolutePath();
         String whatsappImagesPath = "/WhatsApp/Media/WhatsApp Images";
         String finalPath = externalStorageDirectory + whatsappImagesPath;
+        // Initializing(Paths2):
+        String whatsappImagesPath2 = "Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Images";
+        String finalPath2 = externalStorageDirectory + whatsappImagesPath2;
+        // FieldsField:
+        File[] backupFiles = new File(finalPath2).listFiles(file -> isImage(file.getAbsolutePath()));
+        File[] files = new File(finalPath).listFiles(file -> isImage(file.getAbsolutePath()));
+        // Checking:
+        if (files == null || files.length <= 0) files = backupFiles;
         // Returning:
-        return new File(finalPath).listFiles(file -> isImage(file.getAbsolutePath()));
+        return files;
     }
 
     public List<Bitmap> getWhatsappImagesBitmaps() {
