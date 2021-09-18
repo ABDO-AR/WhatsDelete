467472489933bbe2c624d0ca4fa727aa1b18ab0b
ABDO-AR <73670215+ABDO-AR@users.noreply.github.com>
2021-09-18 05:55:54 +0200

    ------------------------------ FRAGMENT: VOICE-STABLE(&WORKING) ------------------------------

--- app/src/main/java/com/ar/team/company/app/whatsdelete/control/adapter/VoicesAdapter.java
+++ app/src/main/java/com/ar/team/company/app/whatsdelete/control/adapter/VoicesAdapter.java
@@ -1,19 +1,27 @@
 package com.ar.team.company.app.whatsdelete.control.adapter;
 
+import android.app.Activity;
 import android.content.Context;
 import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.ar.team.company.app.whatsdelete.R;
 import com.ar.team.company.app.whatsdelete.databinding.VoiceItemViewBinding;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Locale;
@@ -24,11 +32,15 @@ public class VoicesAdapter extends RecyclerView.Adapter<VoicesAdapter.VoicesView
     // Fields:
     private final Context context;
     private final List<File> files;
+    // Fields(MediaPlayer):
+    private int index = 9999999;
+    private MediaPlayer player;
 
     // Constructor:
     public VoicesAdapter(Context context, List<File> files) {
         this.context = context;
         this.files = files;
+        this.player = new MediaPlayer();
         notifyDataSetChanged();
     }
 
@@ -53,11 +65,58 @@ public class VoicesAdapter extends RecyclerView.Adapter<VoicesAdapter.VoicesView
         // Initializing(MediaMetadataRetriever):
         retriever.setDataSource(context, Uri.parse(file.getAbsolutePath()));
         String duration = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+        // Checking:
+        if (position == index)
+            holder.binding.playVoiceButton.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_pause));
+        else
+            holder.binding.playVoiceButton.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_play));
         // Developing:
         holder.binding.durationTextView.setText(formatsMilliSeconds(Long.parseLong(duration)));
         holder.binding.dateTextView.setText(format.format(file.lastModified()));
+        holder.binding.playVoiceButton.setOnClickListener(v -> audioMethod(holder.binding, position));
     }
 
+    // Method(AUDIO):
+    private void audioMethod(VoiceItemViewBinding binding, int position) {
+        // Initializing:
+        String audioPath = files.get(position).getAbsolutePath();
+        // Checking:
+        if (!player.isPlaying()) {
+            // IT'S NOT PLAYING:
+            try {
+                // Initializing:
+                player.setDataSource(audioPath);
+                player.prepare();
+                player.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // SettingRes:
+            binding.playVoiceButton.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_pause));
+        } else {
+            // IT'S ALREADY PLAYING:
+            try {
+                // Stopping:
+                player.stop();
+                // Initializing:
+                player = new MediaPlayer();
+                // Checking:
+                if (index != position) audioMethod(binding, position);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            // SettingRes:
+            binding.playVoiceButton.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_play));
+        }
+        // Setting:
+        index = position;
+        // Notify:
+        notifyDataSetChanged();
+        // Checking:
+        if (index != position) audioMethod(binding, position);
+    }
+
+    // Formatting:
     public static String formatsMilliSeconds(long milliseconds) {
         // Initializing:
         String finalTimerString = "";
@@ -106,5 +165,9 @@ public class VoicesAdapter extends RecyclerView.Adapter<VoicesAdapter.VoicesView
 
     public List<File> getFiles() {
         return files;
+    }
+
+    public MediaPlayer getPlayer() {
+        return player;
     }
 }
